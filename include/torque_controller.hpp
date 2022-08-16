4e9a787cf91ccb4a2d55de0b76e0e0fd616a3e1d
quantumspawner <jet22854111@gmail.com>
2022-08-17 03:14:57 +0800

    fix for node state

--- include/torque_controller.hpp
+++ include/torque_controller.hpp
@@ -10,6 +10,7 @@
 
 // ROS message include
 #include "can_msgs/Frame.h"
+#include "std_msgs/Bool.h"
 
 // NTURT include
 // CAN parser
@@ -21,12 +22,6 @@ class TorqueController {
     public:
         TorqueController(std::shared_ptr<ros::NodeHandle> &_nh);
 
-        /// \brief Callback function when receiving message form topic "sent_messages"
-        void onCan(const can_msgs::Frame::ConstPtr &_msg);
-
-        /// \brief Callback function to shutdown the node
-        void shutdown();
-
         /// \brief Function for testing purposes
         void test();
 
@@ -34,6 +29,12 @@ class TorqueController {
         void test1();
 
     private:
+        /// \brief Callback function when receiving message form topic "sent_messages"
+        void onCan(const can_msgs::Frame::ConstPtr &_msg);
+
+        /// \brief Callback function when receiving message from topic "node_state"
+        void onState(const std_msgs::Bool::ConstPtr &_msg);
+
         /// \brief Pointer to ros node handle
         std::shared_ptr<ros::NodeHandle> nh_;
         
@@ -43,11 +44,17 @@ class TorqueController {
         /// \brief Subscriber to can message
         ros::Subscriber can_sub_;
 
+        /// \brief Subscriber to node state
+        ros::Subscriber state_sub_;
+
         /// \brief Can parser
         Parser parser_;
 
         /// \brief Torque command
         double torque_cmd_;
+
+        /// \brief Internal state to check if initialize is done
+        bool state_ = false;
 };
 
 #endif // TORQUE_CONTROLLER_HPP
--- src/torque_controller.cpp
+++ src/torque_controller.cpp
@@ -2,7 +2,8 @@
 
 TorqueController::TorqueController(std::shared_ptr<ros::NodeHandle> &_nh) : 
                                    nh_(_nh) , mcu_pub_(_nh->advertise<can_msgs::Frame>("sent_messages", 10)),
-                                   can_sub_(_nh->subscribe("received_messages", 10, &TorqueController::onCan, this)) {
+                                   can_sub_(_nh->subscribe("received_messages", 10, &TorqueController::onCan, this)),
+                                   state_sub_(_nh->subscribe("node_state", 10, &TorqueController::onState, this)) {
     // Initialize can parser
     parser_.init_parser();
 
@@ -37,20 +38,21 @@ void TorqueController::onCan(const can_msgs::Frame::ConstPtr &_msg) {
             can_msg.header.stamp = _msg->header.stamp;
             torque_cmd_ = parser_.get_afd("THR", "A");
             // std::cout << torque_cmd_ << std::endl;
-            parser_.set_tbe("MTC", "N", torque_cmd_ * 130);
+            // if ready to drive
+            if (state_) {
+                parser_.set_tbe("MTC", "N", torque_cmd_ * 130);
+            }
+            else {
+                parser_.set_tbe("MTC", "N", 0);
+            }
             parser_.encode(_CAN_MCM, can_msg.data);
             mcu_pub_.publish(can_msg);
         }
     }
 }
 
-void TorqueController::shutdown() {
-    can_msgs::Frame can_msg;
-    can_msg.id = _CAN_MCM;
-    can_msg.header.stamp = ros::Time::now();
-    parser_.set_tbe("MIE", "N", 0);
-    parser_.encode(_CAN_MCM, can_msg.data);
-    mcu_pub_.publish(can_msg);
+void TorqueController::onState(const std_msgs::Bool::ConstPtr &_msg) {
+    state_ = _msg->data;
 }
 
 void TorqueController::test() {
@@ -58,7 +60,12 @@ void TorqueController::test() {
     can_msg.id = _CAN_MCM;
     can_msg.header.stamp = ros::Time::now();
     // test message
-    parser_.set_tbe("MTC", "N", 130);
+    if (state_) {
+        parser_.set_tbe("MTC", "N", 130);
+    }
+    else {
+        parser_.set_tbe("MTC", "N", 0);
+    }
     parser_.encode(_CAN_MCM, can_msg.data);
     // mcu_pub_.publish(can_msg);
 
